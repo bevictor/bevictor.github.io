c87690ce3a1e9e6b00a4449523ee311484084f5f
bevictor <otienovo035@gmail.com>
2021-01-17 16:19:57 +0300

    nav menu

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // toggle menu - needs refining
 const menu = document.querySelector('.menu');
-const nav = document.querySelector('.navigation');
+const nav = document.querySelector('nav');
 menu.addEventListener('click',toggleNav);
 function toggleNav () {
-    if (nav.classList.contains('navigation')) {
-        nav.classList.remove('navigation');
+    if (nav.classList.contains('hidden')) {
+        nav.classList.remove('hidden');
     } else {
-        nav.classList.add('navigation');
+        nav.classList.add('hidden');
     }
 }
