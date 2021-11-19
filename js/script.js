7521f840521a994e9a6b5b382b8f7146ba0cf224
bevictor <otienovo035@gmail.com>
2021-11-19 23:36:20 +0300

    deleted

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// show menu
-const menuIcon = document.querySelector('.nav-icon');
-const navMenu = document.querySelector('.navigation');
-
-menuIcon.addEventListener('click',() => {
-    navMenu.classList.toggle('navigation');
-})
-// swup integration
-const swup = new Swup();
